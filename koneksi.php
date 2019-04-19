f9d834cf57dadf286a61337fd553e605553ef44f
shin95 <jikano95@gmail.com>
2019-04-19 11:27:47 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
   // buat koneksi dengan database mysql
-  $host = "localhost";
-  $user = "root";
-  $pass = "";
-  $name = "crud_sederhana";
+  $host = "bdgweb1.database.windows.net";
+  $user = "admin123";
+  $pass = "B4andung_95";
+  $name = "DbWeb";
   $link = mysqli_connect($host,$user,$pass,$name);
   //periksa koneksi, tampilkan pesan kesalahan jika gagal
   if(!$link){
